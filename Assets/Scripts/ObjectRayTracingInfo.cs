16f2c5e580c9b2c21a4af345eceb0ba7fde7a4b9
Adam Joyce <adamjoyce@outlook.com>
2016-05-29 15:50:53 +0100

    Added ray tracing info class for objects in the scene

---
+++ Assets/Scripts/ObjectRayTracingInfo.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectRayTracingInfo : MonoBehaviour {
+
+    // Three main coefficients.
+    public float lambertCoeff = 1f;
+    public float reflectiveCoeff = 0f;
+    public float transparentCoeff = 0f;
+
+    // Phong variables.
+    public float phongCoeff = 1f;
+    public float phongPower = 2f;
+
+    // Blinn variables.
+    public float blinnPhongCoeff = 1f;
+    public float blinnPhongPower = 2f;
+
+    public Color defaultColor = Color.gray;
+
+    void Awake() {
+        // Assign the material if non exists.
+        if (!GetComponent<Renderer>().material.mainTexture) {
+            GetComponent<Renderer>().material.color = defaultColor;
+        }
+    }
+}
