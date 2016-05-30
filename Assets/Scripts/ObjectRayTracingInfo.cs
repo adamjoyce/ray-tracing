ffa0cd3677b61839c80ada0e92372a80f246f7b3
Adam Joyce <adamjoyce@outlook.com>
2016-05-30 13:18:02 +0100

    Written the main RayTrace() function

--- Assets/Scripts/ObjectRayTracingInfo.cs
+++ Assets/Scripts/ObjectRayTracingInfo.cs
@@ -4,16 +4,16 @@ using System.Collections;
 public class ObjectRayTracingInfo : MonoBehaviour {
 
     // Three main coefficients.
-    public float lambertCoeff = 1f;
-    public float reflectiveCoeff = 0f;
-    public float transparentCoeff = 0f;
+    public float lambertCoefficient = 1f;
+    public float reflectiveCoefficient = 0f;
+    public float transparentCoefficient = 0f;
 
     // Phong variables.
-    public float phongCoeff = 1f;
+    public float phongCoefficient = 1f;
     public float phongPower = 2f;
 
     // Blinn variables.
-    public float blinnPhongCoeff = 1f;
+    public float blinnPhongCoefficient = 1f;
     public float blinnPhongPower = 2f;
 
     public Color defaultColor = Color.gray;
--- Assets/Scripts/RayTracer.cs
+++ Assets/Scripts/RayTracer.cs
@@ -3,13 +3,40 @@ using System.Collections;
 
 public class RayTracer : MonoBehaviour {
 
-	// Use this for initialization
+    public float resolution = 1.0f;
+    public int finalRecursiveIteration = 4;
+
+    private Light[] lights;
+    private Texture2D renderTexture;
+
+    // Setup the lights and render texture for the scene.
+    private void Awake() {
+        lights = FindObjectsOfType(typeof(Light)) as Light[];
+
+        int textureWidth = (int)(Screen.width * resolution);
+        int textureHeight = (int)(Screen.height * resolution);
+        renderTexture = new Texture2D(textureWidth, textureHeight);
+    }
+
+	// Use this for initialization.
 	void Start () {
-	
+        RayTrace();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Cast rays from the camera to each pixel in the scene and set the render texture pixels accordingly.
+    private void RayTrace() {
+        Color colour = Color.black;
+        for (int x = 0; x < renderTexture.width; x++) {
+            for (int y = 0; y < renderTexture.height; y++) {
+                Vector3 rayPosition = new Vector3(x / resolution, y / resolution, 0);
+                Ray ray = GetComponent<Camera>().ScreenPointToRay(rayPosition);
+                //renderTexture.SetPixel(x, y, TraceColour());
+            }
+        }
+    }
+
+    //
+    private void TraceColour(Ray ray, Color colour, int recursiveIteration) {
+
+    }
 }
